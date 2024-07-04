86fc992ff8ec78b1b858b77f66d7c2f2740dfb86
RoshanKumar404 <roshankuar951@gmail.com>
2024-07-04 15:35:42 +0530

    working on ui

--- App.tsx
+++ App.tsx
@@ -1,27 +1,36 @@
-import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Twitter from './src/Screens/MainScreen'
-import tweets from './src/assets/tweets'
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Twitter from './src/Screens/MainScreen';
+import tweets from './src/assets/tweets';
 
 export default function App() {
   return (
-    <View>
-      {/* <ScrollView>
-      <Twitter tweet={tweets[0]}/>
-      <Twitter tweet={tweets[1]}/>
-      <Twitter tweet={tweets[2]}/>
-      <Twitter tweet={tweets[3]}/>
-      <Twitter tweet={tweets[4]}/>
-      <Twitter tweet={tweets[5]}/>
-      <Twitter tweet={tweets[6]}/>
-      <Twitter tweet={tweets[7]}/>
-      </ScrollView> */}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Twitter</Text>
+      </View>
       <FlatList 
-      data={tweets}
-      renderItem={({item})=><Twitter tweet={item}/>}
+        data={tweets}
+        renderItem={({item}) => <Twitter tweet={item} />}
+        keyExtractor={(item) => item.id.toString()}
       />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    padding: 20,
+    backgroundColor: '#1DA1F2',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
