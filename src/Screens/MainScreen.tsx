c24a3ab185049c290e2248f6c55043540db6791b
RoshanKumar404 <roshankuar951@gmail.com>
2024-07-09 23:07:31 +0530

    modified

--- src/Screens/MainScreen.tsx
+++ src/Screens/MainScreen.tsx
@@ -1,37 +1,16 @@
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import tweets from '../assets/tweets'
-import Entypo from '@react-native-vector-icons/entypo';
-import IconButtons from '../assets/IconButton';
 
 export default function Twitter(props) {
     const tweet=props.tweet
   return (
     <View style={styles.Container}>
-       <Image src={tweet.user.image} style={styles.Imagecontainer} />
-            <View style={styles.datacontainer}>
-              
-            {/* <Link href={`/AllTabs/${tweet.id}`} asChild> */}
-                <Pressable style={{flexDirection:"row",}}>
-                    <Text style={{ fontWeight: "bold", color: "black" }}>{tweet.user.name}</Text>
-                    <Text style={{  color: "grey" }}>  {  tweet.user.username}   .2h</Text>
-                    <Entypo 
-                    name="dots-three-horizontal"
-                     size={18} 
-                     color="grey" 
-                     style={{marginLeft:"auto"}} />
-                </Pressable>
-                {/* </Link> */}
-                <Text>{tweet.content}</Text>
-                {tweet.image && <Image src={tweet.image} style={styles.contentIage} />}
-                <View style={styles.containerFooter}>
-                <IconButtons icon='retweet' text={tweet.numberOfRetweets}/>
-               <IconButtons icon='comment' text={tweet.numberOfComments}/>
-               <IconButtons icon='heart' text={tweet.numberOfLikes||0}/>
-               <IconButtons  icon="chart" text={tweet.impressions ||0}/>
-               <IconButtons icon="share-google" text={tweet.share ||0}/>
-                </View>
-            </View>
+        <Image src={tweet.user.image} style={styles.Imagecontainer}/>
+        <View style={styles.datacontainer}>
+        <Text style={{fontWeight:"bold",color:"black"}}>{tweet.user.username}</Text>
+      <Text>{tweet.content}</Text>
+      </View>
     </View>
   )
 }
@@ -55,9 +34,5 @@ const styles = StyleSheet.create({
       },
       Imagecontainer:{
         width:80,height:80,borderRadius:45
-      } ,
-      containerFooter:{
-        flexDirection:"row",
-        justifyContent:"space-between"
-    }
+      } 
 })
--- src/Screens/TweetDetails.tsx
+++ src/Screens/TweetDetails.tsx
@@ -1,19 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Twitter from '../trial'
-import tweets from '@/assets/data/tweets'
-import { useLocalSearchParams,useGlobalSearchParams } from 'expo-router'
-export default function tweeterr() {
-  const {tweeter}=useLocalSearchParams();
-  console.log(tweeter);
-  
-  const tweet= tweets.find((t)=>t.id===tweeter)
-  if(!tweet){
-    return<Text>Tweet {tweeter} not found </Text>
-  }
+
+export default function TweetDetails() {
   return (
-    
-    <Twitter tweet={tweet} />
+    <View>
+      <Text>TweetDetails</Text>
+    </View>
   )
 }
 
